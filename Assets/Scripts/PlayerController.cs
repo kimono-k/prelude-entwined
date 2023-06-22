bb4ca3f36e0c820743a0e3b4192dd4244855d820
kimono-k <kevinnarain94@gmail.com>
2023-06-22 21:06:36 +0200

    Stop animations add

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
     // Variables
     public Rigidbody2D theRB;
     public float moveSpeed;
+    public Animator myAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,19 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        // Moves the player
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        // Initiate player animation when walking
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        // Saves animation when player stops moving (idle)
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
     }
 }
     
