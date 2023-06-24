ca2e696da099d3d7483c5bd92da3ecaff56e95b2
kimono-k <kevinnarain94@gmail.com>
2023-06-24 23:57:31 +0200

    AreaEntrance add

--- Assets/Scripts/AreaEntrance.cs
+++ Assets/Scripts/AreaEntrance.cs
@@ -10,7 +10,7 @@ public class AreaEntrance : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // The player must exist only then execute this script
+        // The player must exist, only then execute this script
         if (PlayerController.instance != null)
         {
             // Is the connected areaEntrance is equal to the areaTransition name of the player
