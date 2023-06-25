f26e6351828d6e0bc71617e7150fc573bff22eb7
kimono-k <kevinnarain94@gmail.com>
2023-06-25 17:21:56 +0200

    Player + PlayerLoader prefab + Player instantiation script add

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Makes sure player stays in scene
+        if (PlayerController.instance == null) Instantiate(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
