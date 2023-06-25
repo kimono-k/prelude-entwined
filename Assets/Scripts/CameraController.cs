57de8ebf43858581996e164be1fec89f03a04155
kimono-k <kevinnarain94@gmail.com>
2023-06-25 20:23:29 +0200

    Attach main camera to player + position bug player fix

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerController.instance != null)
+        {
+            target = PlayerController.instance.transform; // Data of position player
+        }
+    }
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z); // Main camera stalks the player
+    }
+}
