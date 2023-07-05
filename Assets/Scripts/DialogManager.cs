9629c1d489a06160e909c4386a9a4c67417bd93d
kimono-k <kevinnarain94@gmail.com>
2023-07-05 17:28:05 +0200

    Hardcoded array dialog text add

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+
+    public int currentLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogText.text = dialogLines[currentLine];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
