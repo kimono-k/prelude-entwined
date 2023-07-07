7ee8abb02743d8deb16dd00713c3a6dac4a99cf4
kimono-k <kevinnarain94@gmail.com>
2023-07-07 19:02:27 +0200

    Levelling up system add

--- Assets/Scripts/CharStats.cs
+++ Assets/Scripts/CharStats.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental;
 using UnityEngine;
 
 public class CharStats : MonoBehaviour
 {
-
     public string charName;
     public int playerLevel = 1;
     public int currentExp;
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseExp = 1000;
 
     public int currentHP;
     public int maxHP = 100;
@@ -25,7 +28,14 @@ public class CharStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseExp;
+
+        // Levelling up system (start @ level 2)
+        for(int i = 2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i - 1] * 1.05f);
+        }
     }
 
     // Update is called once per frame
