d55de64b56eaf21b7a2800a465e258df0607ff24
kimono-k <kevinnarain94@gmail.com>
2023-07-07 18:28:40 +0200

    Char stats variables add

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentExp;
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+    public int strength;
+    public int defense;
+    public int weaponPower;
+    public int armorPower;
+    public string equippedWeapon;
+    public string equippedArmor;
+    public Sprite charImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
